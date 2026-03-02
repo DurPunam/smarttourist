5ac09eda8f62e2ec48d45162a648a40fcbe6c427
SAICHARAN1189 <saicharannemaliga06@gmail.com>
2026-03-02 16:20:44 +0530

    Use HashRouter for static deployment compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContextImproved";
 import { WebSocketProvider } from "./contexts/WebSocketContext";
 import { ThemeProvider } from "./components/ThemeProvider";
@@ -40,7 +40,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <HashRouter>
             <AuthProvider>
               <WebSocketProvider>
                 <Routes>
@@ -116,7 +116,7 @@ const App = () => (
                 <AIChatbot />
               </WebSocketProvider>
             </AuthProvider>
-          </BrowserRouter>
+          </HashRouter>
         </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
